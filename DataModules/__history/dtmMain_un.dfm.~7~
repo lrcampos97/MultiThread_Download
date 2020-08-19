b5ae2d77626fa3efb278c9eb2923bd81e69fb6d0
lrcampos97 <campos.luiz97@gmail.com>
2020-08-19 10:35:00 -0300

    Configuração inicial do projeto, adicionando a estrutura de pastas e ajustando o layout

---
+++ DataModules/__history/dtmMain_un.dfm.~7~
@@ -0,0 +1,13 @@
+object dtmMain: TdtmMain
+  OldCreateOrder = False
+  Height = 348
+  Width = 492
+  object Connection: TFDConnection
+    Params.Strings = (
+      'Database=C:\Delphi\MultiThread_Download\Data\DB.db'
+      'DriverID=SQLite')
+    LoginPrompt = False
+    Left = 48
+    Top = 32
+  end
+end
---
+++ MultiThread_Download.dpr
@@ -0,0 +1,16 @@
+program MultiThread_Download;
+
+uses
+  Vcl.Forms,
+  frmMain_un in 'Forms\frmMain_un.pas' {frmMain},
+  dtmMain_un in 'DataModules\dtmMain_un.pas' {dtmMain: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TdtmMain, dtmMain);
+  Application.Run;
+end.
