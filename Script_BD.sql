d63469c8416a3ff38c8bcdad2a5baea11838871d
lrcampos97 <campos.luiz97@gmail.com>
2020-08-20 18:04:05 -0300

    Adicionado script para criação da tabela

---
+++ Script_BD.sql
@@ -0,0 +1,410 @@
+--
+-- File generated with SQLiteStudio v3.2.1 on qui ago 20 18:01:17 2020
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: LOGDOWNLOAD
+CREATE TABLE LOGDOWNLOAD (
+    CODIGO     INTEGER       PRIMARY KEY AUTOINCREMENT
+                             NOT NULL,
+    URL        VARCHAR (600) NOT NULL,
+    DATAINICIO DATE          NOT NULL,
+    DATAFIM    DATE
+);
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            1,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '1899-12-30'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            2,
+                            'ssss',
+                            '2020-08-20',
+                            NULL
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            3,
+                            'ghjhgjghjgh',
+                            '2020-08-20',
+                            NULL
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            4,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            5,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.ee',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            6,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            7,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            8,
+                            'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.8/npp.7.8.8.Installer.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            9,
+                            'http://mirror.mrjester.net/ubuntu/release/18.04.4/ubuntu-18.04.4-desktop-amd64.iso',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            10,
+                            'http://mirror.mrjester.net/ubuntu/release/18.04.4/ubuntu-18.04.4-desktop-amd64.iso',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            11,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            12,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            13,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            14,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            15,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            16,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            17,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            NULL
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            18,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            19,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            20,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            21,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            NULL
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            22,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            NULL
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            23,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            24,
+                            'https://d3fmc8hyqclk8u.cloudfront.net/ont635n&pzt2b/Baixaki_uTorrent.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            25,
+                            'https://www.sqlite.org/2020/sqlite-amalgamation-3330000.zip',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            26,
+                            'https://downloads.sourceforge.net/project/dia-installer/dia-win32-installer/0.97.2/dia-setup-0.97.2-2-unsigned.exe?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdia-installer%2Ffiles%2Flatest%2Fdownload&ts=1597956061',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            27,
+                            'https://downloads.sourceforge.net/project/dia-installer/dia-win32-installer/0.97.2/dia-setup-0.97.2-2-unsigned.exe?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdia-installer%2Ffiles%2Flatest%2Fdownload&ts=1597956061',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            28,
+                            'http://cdn.change-vision.com/files/astah-professional-8_2_0-b743f7-jre-setup.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            29,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+INSERT INTO LOGDOWNLOAD (
+                            CODIGO,
+                            URL,
+                            DATAINICIO,
+                            DATAFIM
+                        )
+                        VALUES (
+                            30,
+                            'https://az764295.vo.msecnd.net/stable/78a4c91400152c0f27ba4d363eb56d2835f9903a/VSCodeUserSetup-x64-1.43.0.exe',
+                            '2020-08-20',
+                            '2020-08-20'
+                        );
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
